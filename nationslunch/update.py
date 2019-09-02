e3c1dd8271774da9ef19c8e115bab5acaef66f8a
Markus <markus.ydreskog@gmail.com>
2019-09-02 18:18:29 +0200

    fixed so update.py truncates the file before updating it so the new values don't get appended to the old

--- nationslunch/update.py
+++ nationslunch/update.py
@@ -11,6 +11,7 @@ names = sheet.sheets[0].col_values(1)
 urls = sheet.sheets[0].col_values(2)
 file = "urls.csv"
 with open(file,'w') as f:
+    f.truncate()
     for i in range(len(names)):
         f.write(f"{names[i]}, {urls[i]}\n")
 
